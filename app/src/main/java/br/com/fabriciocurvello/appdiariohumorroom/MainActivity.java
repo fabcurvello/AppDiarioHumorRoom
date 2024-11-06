60c1d876a83decb7bc0779c12a07d415fa21c10c
Fabricio Curvello <fabricio.desenvolvedor@gmail.com>
2024-11-06 16:11:59 -0300

    MainActivity concluída

--- app/src/main/java/br/com/fabriciocurvello/appdiariohumorroom/MainActivity.java
+++ app/src/main/java/br/com/fabriciocurvello/appdiariohumorroom/MainActivity.java
@@ -1,6 +1,9 @@
 package br.com.fabriciocurvello.appdiariohumorroom;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +12,9 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btRegistrar;
+    private Button btHistorico;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +26,26 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-    }
+
+        btRegistrar = findViewById(R.id.main_activity_bt_registrar);
+        btHistorico = findViewById(R.id.main_activity_bt_historico);
+
+        btRegistrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, RegistroActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        btHistorico.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, HistoricoActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
+    } // fim do onCreate()
 }
