beb81ad178b1903df70a5f9f6a08f9f290df392d
Fabricio Curvello <fabricio.desenvolvedor@gmail.com>
2024-11-05 23:06:12 -0300

    Projeto inicializado, Biblioteca Room catalogada no libs.version.toml e no build.gradle.kts

---
+++ app/build.gradle.kts
@@ -0,0 +1,48 @@
+plugins {
+    alias(libs.plugins.android.application)
+}
+
+android {
+    namespace = "br.com.fabriciocurvello.appdiariohumorroom"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "br.com.fabriciocurvello.appdiariohumorroom"
+        minSdk = 28
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+}
+
+dependencies {
+
+    implementation(libs.appcompat)
+    implementation(libs.material)
+    implementation(libs.activity)
+    implementation(libs.constraintlayout)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.ext.junit)
+    androidTestImplementation(libs.espresso.core)
+
+    // Adicionando o Room
+    implementation(libs.room.runtime)
+    annotationProcessor(libs.room.compiler) // Para processar as anotações
+    implementation(libs.room.ktx) // Extensões KTX do Room (opcional)
+}
---
+++ app/src/main/java/br/com/fabriciocurvello/appdiariohumorroom/RegistroDeHumor.java
@@ -0,0 +1,4 @@
+package br.com.fabriciocurvello.appdiariohumorroom;
+
+public class RegistroDeHumor {
+}
