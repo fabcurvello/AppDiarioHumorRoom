2b40dcc4068de04e3a6f01038f12ecb58ef985f3
Fabricio Curvello <fabricio.desenvolvedor@gmail.com>
2024-11-06 10:18:11 -0300

    Classe/Entidade RegistroDeHumor.java finalizada

--- app/src/main/java/br/com/fabriciocurvello/appdiariohumorroom/RegistroDeHumor.java
+++ app/src/main/java/br/com/fabriciocurvello/appdiariohumorroom/RegistroDeHumor.java
@@ -1,4 +1,55 @@
 package br.com.fabriciocurvello.appdiariohumorroom;
 
+import androidx.room.Entity;
+
+import java.util.Date;
+
+@Entity(tableName = "registros_humor")
 public class RegistroDeHumor {
+
+    private int id;
+    private Date data;
+    private int humor;
+    private String motivo;
+
+    // construtor (sem id)
+    public RegistroDeHumor(Date data, int humor, String motivo) {
+        this.data = data;
+        this.humor = humor;
+        this.motivo = motivo;
+    }
+
+    // getters e setters
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Date getData() {
+        return data;
+    }
+
+    public void setData(Date data) {
+        this.data = data;
+    }
+
+    public int getHumor() {
+        return humor;
+    }
+
+    public void setHumor(int humor) {
+        this.humor = humor;
+    }
+
+    public String getMotivo() {
+        return motivo;
+    }
+
+    public void setMotivo(String motivo) {
+        this.motivo = motivo;
+    }
 }
