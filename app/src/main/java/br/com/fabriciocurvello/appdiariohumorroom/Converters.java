7c5fba4058a829488ee0331d81d1622f7d17c76a
Fabricio Curvello <fabricio.desenvolvedor@gmail.com>
2025-06-04 21:23:45 -0300

    Atualizações por conta de upgrade do Android Studio

--- app/src/main/java/br/com/fabriciocurvello/appdiariohumorroom/Converters.java
+++ app/src/main/java/br/com/fabriciocurvello/appdiariohumorroom/Converters.java
@@ -6,11 +6,13 @@ import java.util.Date;
 
 public class Converters {
 
+    //Recebe valor númerico do tipo Long e retorna um Date
     @TypeConverter
     public static Date fromTimestamp(Long value) {
         return value == null ? null : new Date(value);
     }
 
+    //Recebe valor Date e retorna valor numérico tipo Long
     @TypeConverter
     public static Long dateToTimestamp(Date date) {
         return date == null ? null : date.getTime();
